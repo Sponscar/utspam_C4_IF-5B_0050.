f86f1c6205e0fda47ce1c5b7bec17f25db9bd367
Sponscar <panji.sabilillah23@student.uisi.ac.id>
2025-11-22 16:44:44 +0700

    Menambahkan widget CustomInput

---
+++ lib/widgets/custom_input.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class CustomInput extends StatelessWidget {
+  final TextEditingController controller;
+  final String label;
+  final IconData icon;
+  final bool obscureText;
+  final Widget? suffixIcon;
+  final String? Function(String?)? validator;
+
+  const CustomInput({
+    super.key,
+    required this.controller,
+    required this.label,
+    required this.icon,
+    this.obscureText = false,
+    this.suffixIcon,
+    this.validator,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      obscureText: obscureText,
+      validator: validator,
+      decoration: InputDecoration(
+        labelText: label,
+        prefixIcon: Icon(icon),
+        suffixIcon: suffixIcon,
+        filled: true,
+        fillColor: Colors.blue.shade50.withOpacity(0.3),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(14),
+        ),
+      ),
+    );
+  }
+}
