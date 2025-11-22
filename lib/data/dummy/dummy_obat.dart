d76c7dcba4752e544f1abe7589f96a5098e2fd36
Sponscar <panji.sabilillah23@student.uisi.ac.id>
2025-11-22 14:47:11 +0700

    Menambahkan dummy data obat

---
+++ lib/data/dummy/dummy_obat.dart
@@ -0,0 +1,39 @@
+import '../models/obat_model.dart';
+
+List<ObatModel> dummyObat = [
+  ObatModel(
+    id: "1",
+    nama: "Paracetamol",
+    kategori: "Analgesik",
+    gambar: "assets/obat/paracetamol.png",
+    harga: 12000,
+  ),
+  ObatModel(
+    id: "2",
+    nama: "Vitamin C 500mg",
+    kategori: "Vitamin",
+    gambar: "assets/obat/vitamin_c.png",
+    harga: 15000,
+  ),
+  ObatModel(
+    id: "3",
+    nama: "Betadine",
+    kategori: "Antiseptik",
+    gambar: "assets/obat/betadine.png",
+    harga: 18000,
+  ),
+  ObatModel(
+    id: "4",
+    nama: "Amoxicillin",
+    kategori: "Antibiotik",
+    gambar: "assets/obat/amoxicillin.png",
+    harga: 25000,
+  ),
+  ObatModel(
+    id: "5",
+    nama: "Minyak Kayu Putih",
+    kategori: "Minyak Oles",
+    gambar: "assets/obat/kayu_putih.png",
+    harga: 16000,
+  ),
+];
